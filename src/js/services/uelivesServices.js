5c9f13f992ca1a1ebe0a4b621da8acee294ec725
dribehance <dom4j.for.networking@gmail.com>
2016-07-01 11:00:56 +0800

    change to uelives,add uelivesServices

---
+++ src/js/services/uelivesServices.js
@@ -0,0 +1,9 @@
+// by dribehance <dribehance.kksdapp.com>
+angular.module("Uelives").factory("userServices", function($rootScope, $http, apiServices, localStorageService, config) {
+	return {
+		// rsa encrypt
+		rsa_key: apiServices._get(angular.extend({}, config.common_params, {
+			url: "key/private_key.pem",
+		})),
+	}
+});
