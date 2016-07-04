8ae7e27cdcb4d586ecb1a6231aa20ae8af86b3b1
XiaoMingLi <1165406082@qq.com>
2016-07-04 20:26:50 +0800

    update industry

--- src/js/controllers/onlineBookingController.js
+++ src/js/controllers/onlineBookingController.js
@@ -1,3 +1,7 @@
 angular.module("Uelives").controller("onlineBookingController", function($scope, errorServices, toastServices, localStorageService, config) {
-
+	$scope.input = {};
+	$scope.input.gender = 1;
+    $scope.select_gender = function(gender) {
+        $scope.input.gender = gender;
+    };
 })
--- src/js/controllers/relatedIndustryController.js
+++ src/js/controllers/relatedIndustryController.js
@@ -1,5 +1,6 @@
-angular.module("Uelives").controller("relatredIndustryController", function($scope, errorServices, toastServices, localStorageService, config) {
-    $scope.tags = ["机械机电","汽车","家电","航空","化工","日常"];
+angular.module("Uelives").controller("relatedIndustryController", function($scope, errorServices, toastServices, localStorageService, config) {
+    $scope.input={}
+    $scope.tags = ["机械机电","汽车","家电","航空","化工","日常","进出口贸易"];
     $scope.select = function(tag) {
 		tag.select = !tag.select;
 	}
