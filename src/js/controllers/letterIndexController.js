660c041fcb00f4c4bd977c5aa9669a7f6aa3bef3
dribehance <dom4j.for.networking@gmail.com>
2016-07-08 15:04:12 +0800

    add hammer support

---
+++ src/js/controllers/letterIndexController.js
@@ -0,0 +1,21 @@
+// by dribehance <dribehance.kksdapp.com>
+angular.module("Uelives").controller("letterIndexController", function($scope, errorServices, toastServices, localStorageService, config) {
+	$scope.$on("onRepeatDone", function() {
+		var letters = $(".letter"),
+			letter = letters.find("span"),
+			letter_height = letter.height();
+		var hammer = new Hammer(letters[0], {
+			touchAction: "pan-x"
+		});
+		hammer.on("pan", function(e) {
+			var offset = Math.round(e.deltaY / letter_height);
+			$scope.navgation_to($(e.target), offset)
+		})
+		$scope.navgation_to = function(elem, offset) {
+			var dest = letter.index(elem) + offset;
+			dest = Math.min(dest, letter.length - 1);
+			dest = Math.max(0, dest);
+			$scope.$emit("letterIndexChange", letter.eq(dest).text())
+		}
+	})
+})
