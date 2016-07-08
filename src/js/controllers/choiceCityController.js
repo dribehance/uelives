cb3f17a42c7458b462ea6ac53bb38c21596b72b7
XiaoMingLi <1165406082@qq.com>
2016-07-08 15:08:05 +0800

    mogified language.json choice

--- src/js/controllers/choiceCityController.js
+++ src/js/controllers/choiceCityController.js
@@ -1,19 +1,36 @@
 angular.module("Uelives").controller("choiceCityController", function($scope, userServices, errorServices, toastServices, localStorageService, config) {
     $scope.input = {}
     $scope.input.choice = '1';
-    $scope.choice = function(n) {
+
+    $scope.select_hot = function(tag) {
+        $scope.input.tag = tag;
+    }
+
+    //A  同类城市列表字体颜色转换
+    $scope.select_city = function(city) {
+        $scope.input.tag = city
+    }
+
+    //获取城市
+    userServices.query_city().then(function(data) {
+        $scope.cities = data;
+    })
+    $scope.query_city = function(n) {
+        userServices.query_city().then(function(data) {
+            $scope.cities = data;
+        })
+        $scope.input.choice = n;
+    }
+    $scope.query_inter_city = function(n) {
+        userServices.query_inter_city().then(function(data) {
+            $scope.cities = data;
+        })
         $scope.input.choice = n;
     }
 
-    // userServices.query_city().then(function(data) {
-    //     $scope.cities = data.city;
-
-    // });
-    $scope.cities = ["北京", "上海", "广州", "深圳", "天津", "厦门", "长沙","呼和浩特", "武汉","成都","杭州","香港","西安","昆明","大连"];
-    $scope.select = function(tag) {
-
-        $scope.input.tag = tag
+    //显示两个字
+    $scope.parse_key = function(k) {
+        return k.substring(0, 2)
     }
 
-    $scope.letters = ["热门", "A", "B", "C", "D", "E", "F","G","H","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
 })
