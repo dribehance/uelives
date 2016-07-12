a72683cd8022ccc78aedb8bfb30566d30edd49b2
XiaoMingLi <1165406082@qq.com>
2016-07-12 18:21:43 +0800

    modified scenes self_booking

--- src/js/controllers/selfBookingController.js
+++ src/js/controllers/selfBookingController.js
@@ -2,6 +2,18 @@ angular.module("Uelives").controller("selfBookingController", function($scope, e
     $scope.input = {};
     $scope.input.gender = 1;
     $scope.select_gender = function(gender) {
+    	
         $scope.input.gender = gender;
     };
+    $scope.model = {
+        status: 0
+    };
+    $scope.go = function() {
+
+        $scope.model.status = 1;
+    }
+    $scope.cancel = function() {
+        $scope.model.status = 0;
+    }
+    
 })
