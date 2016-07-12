c20aa3aa64ece88114df4b04e894b77e6e1b1ff3
XiaoMingLi <1165406082@qq.com>
2016-07-12 08:40:34 +0800

    modified edit information  order

--- src/js/controllers/editBasicInfoController.js
+++ src/js/controllers/editBasicInfoController.js
@@ -1,13 +1,17 @@
 angular.module("Uelives").controller("editBasicInfoController", function($scope, errorServices, toastServices, localStorageService, config) {
-	$scope.input={
+    $scope.input = {
 
-	}
-	$scope.input.gender = 1;
-	$scope.select_gender=function(n){
-		$scope.input.gender=n;
-	}
-	$scope.input.type = 1;
-	$scope.select_type=function(n){
-		$scope.input.type=n;
-	}
+    }
+    $scope.input.options = {};
+    $scope.input.gender = 1;
+    $scope.select_gender = function(n) {
+        $scope.input.gender = n;
+    }
+    $scope.input.type = 1;
+    $scope.select_type = function(n) {
+        $scope.input.type = n;
+    }
+    
+    $scope.degrees = ["请选择", "大专", "本科", "硕士", "博士"];
+    $scope.input.degree = $scope.degrees[0]
 })
