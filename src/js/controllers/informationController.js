f7a1a45939a95cec83ea44529637e22fb2a73e78
dribehance <dom4j.for.networking@gmail.com>
2016-07-13 10:32:32 +0800

    query basic infor

--- src/js/controllers/informationController.js
+++ src/js/controllers/informationController.js
@@ -1,5 +1,16 @@
-angular.module("Uelives").controller("informationController", function($scope, userServices, errorServices, toastServices, localStorageService, config) {
+angular.module("Uelives").controller("informationController", function($scope, $filter, userServices, errorServices, toastServices, localStorageService, config) {
 	$scope.input = {};
+	toastServices.show();
+	userServices.query_basicinfo({
+		type: "1",
+	}).then(function(data) {
+		toastServices.hide()
+		if (data.code == config.request.SUCCESS && data.status == config.response.SUCCESS) {
+			$scope.user = data.Result.UserInfo;
+		} else {
+			errorServices.autoHide(data.message);
+		}
+	})
 	$scope.input.flow = {};
 	$scope.update_avatar = function() {
 		toastServices.show();
@@ -14,15 +25,7 @@ angular.module("Uelives").controller("informationController", function($scope, u
 			}
 		})
 	}
-	toastServices.show();
-	userServices.query_basicinfo({
-		type: "1",
-	}).then(function(data) {
-		toastServices.hide()
-		if (data.code == config.request.SUCCESS && data.status == config.response.SUCCESS) {
-			$scope.user = data.Result.UserInfo;
-		} else {
-			errorServices.autoHide(data.message);
-		}
-	})
+	$scope.format_time = function(time, format) {
+		return $filter("date")(time, format)
+	}
 })
