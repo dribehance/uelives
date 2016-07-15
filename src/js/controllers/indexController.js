01335580b49a813bd33352b19bb91c3c111d900c
dribehance <dom4j.for.networking@gmail.com>
2016-07-15 09:12:36 +0800

    add translator list

--- src/js/controllers/indexController.js
+++ src/js/controllers/indexController.js
@@ -1,5 +1,5 @@
 // by dribehance <dribehance.kksdapp.com>
-angular.module("Uelives").controller("indexController", function($scope, $filter, $location, errorServices, toastServices, localStorageService, config) {
+angular.module("Uelives").controller("indexController", function($scope, $filter, $location, $timeout, userServices, errorServices, toastServices, localStorageService, config) {
 	$scope.input = {};
 	$scope.input.city = "深圳";
 	$scope.input.language = {
@@ -28,5 +28,25 @@ angular.module("Uelives").controller("indexController", function($scope, $filter
 		$scope.input.choice_time && ($scope.input.schedule_from = $scope.format_time($scope.input.choice_time[0], "MM-dd"));
 		$scope.input.choice_time && ($scope.input.schedule_to = $scope.format_time($scope.input.choice_time[$scope.input.choice_time.length - 1], "MM-dd"));
 		$scope.input.choice_time && ($scope.input.schedule_total = $scope.input.choice_time.length)
+	}
+	$scope.ajaxForm = function() {
+		toastServices.show();
+		userServices.booking({
+			city: $scope.input.city,
+			from_language: $scope.input.language.from,
+			to_language: $scope.input.language.to,
+			translate_scene: $scope.input.scenes,
+			order_time: $scope.input.choice_time.join("#"),
+			translate_day: $scope.input.schedule_total,
+			translate_field: $scope.input.industry,
+			sex: $scope.input.gender,
+		}).then(function(data) {
+			toastServices.hide()
+			if (data.code == config.request.SUCCESS && data.status == config.response.SUCCESS && data.have_translate_user == '1') {
+				$location.path("interpreter_list")
+			} else {
+				$location.path("self_booking");
+			}
+		})
 	}
 })
