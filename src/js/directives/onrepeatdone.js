08c6a7161cd460ba90a915cd42b9a8d4b1523504
dribehance <dom4j.for.networking@gmail.com>
2016-07-16 18:02:57 +0800

    remove hiden on repeat done

--- src/js/directives/onrepeatdone.js
+++ src/js/directives/onrepeatdone.js
@@ -2,9 +2,7 @@ angular.module("Uelives").directive('onRepeatDone', function() {
 	return {
 		restrict: 'A',
 		link: function(scope, el, attr) {
-			$(el).hide();
 			if (scope.$last) {
-				$(el).show();
 				scope.$emit("onRepeatDone", el);
 			}
 		}
